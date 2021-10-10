ede11ee7202e76cfed7b90d2d11aa7766a0e917d
bobowang(王波) <bobowang@webank.com>
2021-10-10 15:44:57 +0800

    init

---
+++ spring-introduction/src/main/java/com/example/spring/entity/UserDetail.java
@@ -0,0 +1,14 @@
+package com.example.spring.entity;
+
+/**
+ * @author bobo
+ * @date 2021/10/7
+ * @desc
+ */
+public class UserDetail extends User{
+    private String address;
+
+    public String getAddress() {
+        return address;
+    }
+}
